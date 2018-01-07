10f97ad35b50376ec0aa64a739cb160da88b6337
Wellison <wellison28@gmail.com>
2018-01-07 06:10:20 -0200

    Applyng validation to Restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,13 @@
 class Restaurant < ApplicationRecord
+	validates_presence_of :name, message: " - deve ser preenchido"
+	validates_presence_of :address, message: " - deve ser preenchido"
+	validates_uniqueness_of :name, message: "nome já cadastrado"
+	validates_uniqueness_of :address, message: "endereço já cadastrado"
+	validate :primeira_letra_maiuscula
+
+	private 
+	def primeira_letra_maiuscula
+		unless nam =~ /[A-Z].*/
+			errors.add(:name, "primeira letra deve ser maiúscula")
+		end
 end
