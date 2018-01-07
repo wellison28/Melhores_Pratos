f52836e9c963d8c64ecb0d8511ead361353fed51
Wellison <wellison28@gmail.com>
2018-01-07 05:56:10 -0200

    creating Restaurant model

---
+++ db/migrate/20180107074816_create_restaurants.rb
@@ -0,0 +1,10 @@
+class CreateRestaurants < ActiveRecord::Migration[5.1]
+  def change
+    create_table :restaurants do |t|
+    	t.string :name, limit: 80, null: false
+    	t.string :address, limit: 150, null: false
+    	t.string :specialty, limit: 80
+      t.timestamps
+    end
+  end
+end
