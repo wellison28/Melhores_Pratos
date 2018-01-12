91c6ea675cdeb8c46b22bcb056c6250e5fb65fd2
Wellison <wellison28@gmail.com>
2018-01-12 09:07:57 -0200

    fixing qualification model

--- app/models/qualification.rb
+++ app/models/qualification.rb
@@ -1,11 +1,12 @@
 class Qualification < ApplicationRecord
 	validates_presence_of :score, message: " - deve ser preenchido"
 	
-	validates_numericality_of :score, 
+		validates_numericality_of :score, 
 														greater_than_or_equal_to: 0, 
-											      less_than_or_equal_to: 10
+											      less_than_or_equal_to: 10,
 														message: " - deve ser um numero entre 0 e 10"
 	
-	validates_numericality_of :amount_spent, greter_than: 0,
+	validates_numericality_of :amount_spent, 
+														greater_than: 0,
 														message: " - deve ser um numero maior que 0"
 end
