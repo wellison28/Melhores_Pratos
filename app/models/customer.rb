0b756e2aa636d5083cfb749a989f1f4388f332f4
Wellison <wellison28@gmail.com>
2018-01-16 09:16:49 -0200

    Refactory Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,10 @@
 class Customer < ApplicationRecord
 	has_many :qualifications
 
-  validates_presence_of :name, message: ' - deve ser preenchido'
-  validates_uniqueness_of :name, message: ' - nome ja cadastrado'
-  validates_numericality_of :age, greater_than: 0,
-                                  less_than: 100,
-                                  message: ' - deve ser um numero entre 0 e 100'
+  validates :name, 
+  					presence: true, 
+  					uniqueness: true
+  					
+ validates :age, numericality: {greater_than: 0, less_than: 100} 
+
 end
