076234a8db4a9b38f7994880bb2f4e828387c092
Wellison <wellison28@gmail.com>
2018-01-16 09:17:13 -0200

    Creating tests Customer model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	
+	it 'Has a factory valid' do
+		expect(build(:customer)).to be_valid
+	end
+
+  it { should have_many(:qualifications) }
+  it { should validate_presence_of(:name)}
+  it { should validate_uniqueness_of(:name)}
+  it { should validate_numericality_of(:age).
+  												 is_less_than(100).
+  												 is_greater_than(0)}
 end
