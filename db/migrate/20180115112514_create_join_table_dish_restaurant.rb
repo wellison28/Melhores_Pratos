969b29cd04c3be05e5604fb9e69256d8e8774470
Wellison <wellison28@gmail.com>
2018-01-23 07:03:18 -0200

    fixing join table

--- db/migrate/20180115112514_create_join_table_dish_restaurant.rb
+++ db/migrate/20180115112514_create_join_table_dish_restaurant.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableDishRestaurant < ActiveRecord::Migration[5.1]
   def change
-    create_join_table :categories, :products do |t|
-      t.index [:category_id, :product_id]
+    create_join_table :dishes, :restaurants do |t|
+      t.index [:dish_id, :restaurant_id]
     end
   end
 end
