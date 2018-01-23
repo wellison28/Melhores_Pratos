28538896e4d00b9e61a2725cbfd73e5ce2243fb0
Wellison <wellison28@gmail.com>
2018-01-23 07:55:53 -0200

    Creating tests models

--- spec/models/qualification_spec.rb
+++ spec/models/qualification_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Qualification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a factory valid' do
+  	q = build(:qualification)
+  	q = build(:restaurant)
+  	q = build(:customer)
+   	expect(q).to be_valid
+  end
+
+  it { should belong_to(:customer)}
+  it { should belong_to(:restaurant)}
+  it { should validate_presence_of(:score)}
+  it { should validate_presence_of(:customer_id)}
+  it { should validate_presence_of(:restaurant_id)}
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'has a factory valid' do
+  	create(:restaurant)
+  	r = build(:recipe)
+  	r.dish = create(:dish)
+  	expect(r).to be_valid
+  end
+
+  it { should belong_to(:dish)}
+  it { should validate_presence_of(:content)}
+  it { should validate_presence_of(:dish_id)}
+  	
 end
