8a35c470a6f9b87609d29d4d676442e7d97eec77
Wellison <wellison28@gmail.com>
2018-01-23 07:01:42 -0200

    creating tests

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it 'has a factory valid' do
+		create(:restaurant)
+		expect(build(:dish)).to be_valid
+	end
+
+  it { should have_one(:recipe) }
+  it { should have_and_belong_to_many(:restaurants) }
+  it { should validate_presence_of(:name) }
+	
+	it 'validate presence of more than one restaurant' do
+		expect(build(:dish)).to be_invalid
+	end
+
 end
